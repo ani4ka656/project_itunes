4687d1590844f4e585a5e59e2f9d75253224b70f
ani4ka656 <annavasileva_@abv.bg>
2018-03-18 09:27:50 +0200

    sixt commit

--- index.php
+++ index.php
@@ -13,24 +13,24 @@ if(isset($_POST['user_name']) && isset($_POST['user_password'])){
 		echo '<div>'; 
 		$read_query = "SELECT * FROM `songs` JOIN singers ON songs.singer_id=singers.singer_id JOIN users_info ON songs.user_id=users_info.user_id WHERE date_deleted IS NULL";
 		$result= mysqli_query($conn,$read_query);
+		echo "<table border=1 class='table table-striped'>";
+		echo "<tr>";
+		echo "<td>Song id</td>";
+		echo "<td>Song name </td>";
+		echo "<td>Song url</td>";
+		echo "<td>Song artist</td>";
+		echo "<td>Date of publishing</td>";
+		echo "<td>User who published it</td>";
+		echo "<td>Rate</td>";
+		echo "<td>Update</td>";
+		echo "<td>Delete</td>";
+		echo "<td>Rate</td>";
+		echo "</tr>";
 		if(mysqli_num_rows($result)>0){
 			//$one_row = mysqli_fetch_assoc($result);
 			//echo "<div class='btn btn-info'><a href='create.php?user_id=" .$one_row['user_id']. "'>ADD UNIT</a></div>";
 			while($row = mysqli_fetch_assoc($result)){
 				//echo "<div class='btn btn-info'><a href='create.php?user_id=" .$row['user_id']. "'>ADD UNIT</a></div>"; 
-				echo "<table border=1 class='table table-striped'>";
-				echo "<tr>";
-			echo "<td>Song id</td>";
-			echo "<td>Song name </td>";
-			echo "<td>Song url</td>";
-			echo "<td>Song artist</td>";
-			echo "<td>Date of publishing</td>";
-			echo "<td>User who published it</td>";
-			echo "<td>Rate</td>";
-			echo "<td>Update</td>";
-			echo "<td>Delete</td>";
-			echo "<td>Rate</td>";
-			echo "</tr>";
 				echo "<tr class='info'>";
 				echo "<td>" . $row['song_id'] . ' </td><td>'.$row['song_name'] . '</td><td>'.$row['song_url'] . '</td><td>'.$row['singer_name'] . '</td><td>'.$row['date_of_publishing'] . '</td><td>'.$row['user_name'] . '</td><td>'.$row['rate'] . '</td>';
 
@@ -39,7 +39,7 @@ if(isset($_POST['user_name']) && isset($_POST['user_password'])){
 				echo "<td><a href='rate.php?song_id=" . $row['song_id'] . "'>Rate</a></td>";
 				echo "</tr>";
 			}
-				echo "</table>";
+			echo "</table>";
 				echo '</div>';		}
 	}  else {
 			echo "<a href='logggingin.php'>Try again</a>";
